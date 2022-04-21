88fa7e86b69e9e24e14dda5f492e8d0e47a8d437
Ram Pandey <ram.pandey22@outlook.com>
2022-04-21 23:44:55 +1000

    removed comments

--- 1_infrastructure/provider.tf
+++ 1_infrastructure/provider.tf
@@ -13,23 +13,3 @@ provider "aws" {
   profile = var.aws_profile
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# AWS PROVIDER USING TF CLI
-# ---------------------------------------------------------------------------------------------------------------------
-
-# provider "aws" {
-#   profile = "default-ecs"
-#   version = "~> 2.25"
-#   region  = "${var.aws_region}"
-# }
-
-# terraform {
-#   required_version = "~>0.12"
-#   backend "remote" {
-#     organization = "aws-isv"
-
-#     workspaces {
-#       name = "petclinic-fargate"
-#     }
-#   }
-# }
